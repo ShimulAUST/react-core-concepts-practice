e16c0bd16c6f25ab72a43aa7d45eb5a2e7967498
ShimulAUST <shimulpaul59@gmail.com>
2021-09-18 21:36:58 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const singer = {
+    name: "Shimul",
+    job: "Singer"
+  }
+  const singerStyle = {
+    backgroundColor: 'red'
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +17,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p style={singerStyle}>This is singer {singer.name} and his job is {singer.job}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,9 +26,22 @@ function App() {
         >
           Learn React
         </a>
+        <p>This is Shimul</p>
+        <Person name="Rubel" nayika="Mousumi"></Person>
+        <Person name="Karim" nayika="Raba"></Person>
+        <Person name="Rahim" nayika="Rupban"></Person>
+
       </header>
     </div>
   );
 }
-
+function Person(props) {
+  console.log(props);
+  return (
+    <div className="Person">
+      <h1>Name:{props.name}</h1>
+      <h4>Profession:{props.nayika}</h4>
+    </div>
+  );
+}
 export default App;
